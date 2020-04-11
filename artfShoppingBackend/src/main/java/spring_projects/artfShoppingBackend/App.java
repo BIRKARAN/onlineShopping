0f32920e4a7419c981147fd6ecfc36dc2e6ccf15
BIRKARAN <KARANBIR5401@gmail.com>
2020-04-11 12:33:34 +0530

    commit online shopping

---
+++ artfShoppingBackend/src/main/java/spring_projects/artfShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package spring_projects.artfShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
